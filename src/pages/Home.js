8420f015000a6e17f33a15b91b6b6b7c08ae15d3
Aakash Poudel <aakash.poudel.900@gmail.com>
2024-03-17 11:35:32 -0500

    Whatsapp chat integration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,8 @@ import img1 from "../assets/img1.jpg";
 import img2 from "../assets/img2.jpg";
 import img3 from "../assets/img3.png";
 import "../pages/Home.css";
-
+import WhatsApp from "../assets/WhatsAppButtonGreenLarge.png";
+import { useState } from "react";
 export const Home = () => {
   const carousel = {
     width: "100%",
@@ -19,6 +20,28 @@ export const Home = () => {
     objectFit: "cover",
   };
 
+  const chatIcon = {
+    position: "absolute",
+    bottom: "60px",
+    left: "1700px",
+    zIndex: "1000",
+    cursor: "pointer",
+    transition: "transform 0.2s ease-in-out",
+  };
+  const openWhatsApp = () => {
+    const whatsappURL =
+      "https://wa.me/+9779843713458?text=Hello%2C%20Namaste%21%20I%20have%20some%20questions%3F";
+    window.open(whatsappURL, "_blank");
+  };
+  const [isShaking, setIsShaking] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsShaking(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsShaking(false);
+  };
   return (
     <>
       <div className="carousel" style={carousel}>
@@ -50,6 +73,21 @@ export const Home = () => {
             />
           </Carousel.Item>
         </Carousel>
+        <div
+          className="fixed-bottom right -100 p-3"
+          style={chatIcon}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+            <img
+              src={WhatsApp}
+              width={300}
+              alt="aaa"
+              className={isShaking ? "shake" : ""}
+              onClick={openWhatsApp}
+            />
+
+        </div>
       </div>
     </>
   );
